77a5f5f496ba2cf13d0756c788d921a3e0ab36f5
Rana <r-saber@nlaw.northwestern.edu>
2021-06-20 00:03:30 -0500

    legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -29,24 +29,36 @@ function createMap(quakeInfo) {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-};
 
-for (var i = 0; i < quakeInfo.length; i++) {
+    for (var i = 0; i < quakeInfo.length; i++) {
 
-    var color_scale = chroma.scale(['Blue', 'Green', 'Red']).mode('lab').gamma(2);
-    color = color_scale((quakeInfo[i].dpt / 10)).hex()
+        var color_scale = chroma.scale(['Blue', 'Green', 'Red']).mode('lab').gamma(2);
+        color = color_scale((quakeInfo[i].dpt / 10)).hex()
 
-    var circleLayer = L.circle(quakeInfo[i].coordinates, {
-        color: "Beige",
-        weight: 0.5,
-        fillColor: color,
-        fillOpacity: 0.75,
-        radius: quakeInfo[i].mag * 15000
+        var circleLayer = L.circle(quakeInfo[i].coordinates, {
+            color: "Beige",
+            weight: 0.5,
+            fillColor: color,
+            fillOpacity: 0.75,
+            radius: quakeInfo[i].mag * 15000
 
-    }).bindPopup("<h1>" + quakeInfo[i].place + "</h1> <hr> <h3>Magnitude: " + quakeInfo[i].mag + "   ||   Depth: " + quakeInfo[i].dpt + "</h3>")
-        .addTo(map);
-    circleLayer.on('mouseover', function (e) {
-        e.target.bringToFront();
-    });
-    circleLayer.addTo(map);
+        }).bindPopup("<h1>" + quakeInfo[i].place + "</h1> <hr> <h3>Magnitude: " + quakeInfo[i].mag + "   ||   Depth: " + quakeInfo[i].dpt + "</h3>")
+            .addTo(map);
+        circleLayer.on('mouseover', function (e) {
+            e.target.bringToFront();
+        });
+        circleLayer.addTo(map);
+    };
+
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += "<h4>All Earthquakes from the past 7 Days</h4>";
+        div.innerHTML += '<span><i class="bi bi-circle-fill" style="color: #648FFF"></i> &nbsp;&nbsp; &lt; 5 </span><br>';
+        div.innerHTML += '<span><i class="bi bi-circle-fill" style="color: #785EF0"></i> &nbsp;&nbsp; 5 - 10</span><br>';
+        div.innerHTML += '<span><i class="bi bi-circle-fill" style="color: #DC267F"></i> &nbsp;&nbsp; 10 - 20</span><br>';
+        div.innerHTML += '<span><i class="bi bi-circle-fill" style="color: #FE6100"></i> &nbsp;&nbsp; 20 - 50</span><br>';
+        return div;
+    };
+    legend.addTo(map);
 };
